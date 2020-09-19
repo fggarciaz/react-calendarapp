0bc6757ad5d1799eb61b5130d7cc0a000117f137
Fabricio Garcia <fgarciazerda@gmail.com>
2020-09-19 02:21:46 -0500

    CalendarApp v1

---
+++ src/types/types.js
@@ -0,0 +1,22 @@
+export const types = {
+
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+    eventSetActive: '[event] Set active',
+    eventStartAddNew: '[event] Start add new',
+    eventAddNew: '[event] Add new',
+    eventClearActiveEvent: '[event] Clear Active Event',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+    eventLoaded: '[event] Events loaded',
+    eventClearEvents: '[event] Clear events',
+
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] StartLogin',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start Register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+
+}
